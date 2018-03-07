11aa59497b8a5b03ef562c3c1ab4aca03982a9ab
Loek Vogels <loek.vogels@student.fontys.nl>
2018-03-07 21:24:23 +0100

    Finished Login, Forgot Password and started Homepage UI

---
+++ src/main/java/forgotPasswordController.java
@@ -0,0 +1,21 @@
+import javafx.fxml.FXML;
+
+import javafx.scene.control.*;
+
+import javafx.scene.image.*;
+
+public class forgotPasswordController {
+    @FXML
+    private Button btnSendPassword;
+
+    @FXML
+    private TextField txtfldEmail;
+
+    @FXML
+    private ImageView imgImage;
+
+
+    public void btnSendPasswordClicked(){
+
+    }
+}
---
+++ src/main/java/loginController.java
@@ -0,0 +1,38 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.*;
+
+
+
+public class loginController {
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private Button btnLoginAsGuest;
+
+    @FXML
+    private TextField txtfldUsername;
+
+    @FXML
+    private PasswordField psswrdfldPassword;
+
+    @FXML
+    private Label lblForgotPassword;
+
+    @FXML
+    private ImageView imgImage;
+
+
+    public void btnLoginClicked(){
+
+    }
+
+    public void btnLoginAsGuestClicked(){
+
+    }
+
+    public void lblForgotPasswordClicked(){
+
+    }
+}
