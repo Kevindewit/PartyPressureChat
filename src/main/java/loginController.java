61f8b4b2a7f8826c5751c36c10e3e8090db51ce7
Loek Vogels <loek.vogels@student.fontys.nl>
2018-03-09 09:08:11 +0100

    Added HomepageGuest and HomepageMember and fixed the buttonhandler on loginUI

--- src/main/java/loginController.java
+++ src/main/java/loginController.java
@@ -1,7 +1,11 @@
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
 import javafx.scene.image.*;
+import javafx.scene.*;
+import javafx.stage.*;
 
+import java.io.IOException;
 
 
 public class loginController {
@@ -24,15 +28,43 @@ public class loginController {
     private ImageView imgImage;
 
 
-    public void btnLoginClicked(){
+    public void btnLoginClicked() {
+        try {
+            //Open and show the new homepageMember window
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("homepageMemberUI.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Homepage for Members");
+            stage.setScene(new Scene(root, 450, 450));
+            stage.show();
 
+            //Close the current window
+            Stage thisStage = (Stage) btnLogin.getScene().getWindow();
+            thisStage.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void btnLoginAsGuestClicked(){
+    public void btnLoginAsGuestClicked() {
+        try {
+            //Open and show the new homepageGuest window
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("homepageGuestUI.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Homepage for Guests");
+            stage.setScene(new Scene(root, 450, 450));
+            stage.show();
 
+            //Close the current window
+            Stage thisStage = (Stage) btnLoginAsGuest.getScene().getWindow();
+            thisStage.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void lblForgotPasswordClicked(){
+    public void lblForgotPasswordClicked() {
 
     }
 }
