29799edb37dc9b86dec6d2f79384e0dfd52f60a7
Loek Vogels <loek.vogels@student.fontys.nl>
2018-03-12 15:26:33 +0100

    little bug fixes in forgotPassword

--- src/main/java/forgotPasswordController.java
+++ src/main/java/forgotPasswordController.java
@@ -1,8 +1,14 @@
 import javafx.fxml.FXML;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 
 import javafx.scene.image.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class forgotPasswordController {
     @FXML
@@ -14,8 +20,29 @@ public class forgotPasswordController {
     @FXML
     private ImageView imgImage;
 
+    @FXML
+    private Button btnBack;
+
 
     public void btnSendPasswordClicked(){
 
+    }
+
+    public void btnBackClicked(){
+        try {
+            //Open and show the new homepageMember window
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("loginUI.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Login");
+            stage.setScene(new Scene(root));
+            stage.show();
+
+            //Close the current window
+            Stage thisStage = (Stage) btnBack.getScene().getWindow();
+            thisStage.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
